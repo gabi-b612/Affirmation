7bb654b1911ac1060e822b4483f1c9d59dc11c04
gabi-b612 <20km095@esisalama.org>
2023-08-16 11:05:19 +0200

    first compilation

--- app/src/main/java/com/example/affirmation/MainActivity.kt
+++ app/src/main/java/com/example/affirmation/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.affirmation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.affirmation.databinding.ActivityMainBinding
+import com.example.affirmation.data.Datasource
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.TextView.text = Datasource().loadAffirmations().size.toString()
     }
 }
